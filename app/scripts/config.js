5be47e35f6263d9339266fd79d98d15fe07ff340
Alek Hristov <alek.hristov@yahoo.com>
2018-02-24 23:15:28 +0200

    RandomJoke added to nav and implemented

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -3,5 +3,6 @@ const config = (function configFunc() {
         companyInfoApi: 'https://api.spacexdata.com/v2/info',
         rocketsApi: 'https://api.spacexdata.com/v2/rockets',
         launchesApi:'https://api.spacexdata.com/v2/launches/latest',
+        jokeApi: 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke'
     }
 })();
