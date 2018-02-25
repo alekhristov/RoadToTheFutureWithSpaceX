cbbefb0d56fb0425d8727fa895957430212b6009
Alek Hristov <alek.hristov@yahoo.com>
2018-02-25 02:23:04 +0200

    Rockets images added as links

--- app/scripts/rocketsController.js
+++ app/scripts/rocketsController.js
@@ -1,11 +1,13 @@
 const visualiseRockets = () => {
     $.get(config.rocketsApi)
         .then((result) => {
-            let rocketsData = '<div>';
+            console.log(result);
+            let rocketsData = '<div class="row">';
 
             for (rocket of result) {
+                rocketsData += `<div class="col-md-4"><a href="#" onclick="visualiseRocketDetails(rocket)"><img src="app/images/${rocket.id}.jpg" alt="" class="img-fluid"></a>`
                 rocketsData += `<h1>Rocket name: ${rocket.name}</h1>`;
-                rocketsData += `<h1>Cost per launch: ${rocket.cost_per_launch}</h1>`;
+                rocketsData += `<h1>Cost per launch: ${rocket.cost_per_launch}</h1></div>`;
             }
             rocketsData += '</div>';
             $('#request').html(rocketsData);
@@ -13,6 +15,6 @@ const visualiseRockets = () => {
 
         .catch(function (error) {
             $('#request')
-            .html($(`<h2>An Error has Ocurred</h2>`));
+                .html($(`<h2>An Error has Ocurred</h2>`));
         });
 }
