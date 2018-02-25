2114cc691407bedb4b63415cf5a070ba9dbc3bc9
Alek Hristov <alek.hristov@yahoo.com>
2018-02-25 21:11:33 +0200

    Implemented basic styling for Home and Rockets pages

--- app/scripts/rocketsController.js
+++ app/scripts/rocketsController.js
@@ -3,14 +3,14 @@ $(function () {
         $.get(config.rocketsApi)
             .then((result) => {
                 console.log(result);
-                let rocketsData = '<div class="row">';
+                let rocketsData = '<div><h2 class="home-header">Random Header</h2><div class="row">';
 
                 for (rocket of result) {
-                    rocketsData += `<div class="col-md-4"><a href="#" onclick="visualiseRocketDetails(rocket)"><img src="app/images/${rocket.id}.jpg" alt="" class="img-fluid"></a>`
-                    rocketsData += `<h1>Rocket name: ${rocket.name}</h1>`;
-                    rocketsData += `<h1>Cost per launch: ${rocket.cost_per_launch}</h1></div>`;
+                    rocketsData += `<div class="col-md-4 cols-style"><a href="#" onclick="visualiseRocketDetails(rocket)"><img src="app/images/${rocket.id}.jpg" alt="" class="img-fluid img-hover"></a>`
+                    rocketsData += `<h3>Rocket name: ${rocket.name}</h3>`;
+                    rocketsData += `<h3>Cost per launch: ${rocket.cost_per_launch}</h3></div>`;
                 }
-                rocketsData += '</div>';
+                rocketsData += '</div> </div>';
                 $('#request').html(rocketsData);
             })
 
